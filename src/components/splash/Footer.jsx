4b42c7b080bfcfb22cc5c29e648adde331dd3c24
Abdelmalek Djemaa <121457480+Abdelmalek-Djemaa@users.noreply.github.com>
2024-07-10 13:34:40 +0100

    Update Footer.jsx

--- src/components/splash/Footer.jsx
+++ src/components/splash/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div className="w-full flex text-gray-400 justify-center items-center font-medium sm:text-sm text-xs">
-            Copy Right © 2024 Malek & Raid
+            Copy Right © 2024 Abdelmalek Djemaa
         </div>
     );
 }
